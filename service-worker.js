2ca61d7a1e575ee3503b760ef8226b54aed5aa4a
hanseoll <2hanseoll@gmail.com>
2026-07-12 12:31:03 +0900

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE='hansel-broadcast-stage4-v1';
-const ASSETS=['./','./index.html','./style.css','./script.js','./config.js','./site.webmanifest','./assets/favicon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));});
